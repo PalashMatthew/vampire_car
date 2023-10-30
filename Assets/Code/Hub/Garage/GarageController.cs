ac4352c235cc4cc89b79db2edf8b5c78578759eb
MatthewP <PalashM@mail.ru>
2023-10-30 12:25:43 +0300

    max_scene

--- Assets/Code/Hub/Garage/GarageController.cs
+++ Assets/Code/Hub/Garage/GarageController.cs
@@ -1,10 +1,40 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GarageController : MonoBehaviour
 {
     public GameObject canvasChangeCar;
+
+    [Header("Car Settings")]
+    public ChangeCarController changeCarController;
+    public TMP_Text tCarName;
+    public Image imgCar;
+    public TMP_Text tDamage;
+    public TMP_Text tHealth;
+
+    [Header("Car Level")]
+    public TMP_Text tCarLevel;
+    public Image fillCarLevel;
+    public Image fillEndCarLevel;
+
+    public void Initialize()
+    {
+        tCarName.text = changeCarController._activeCarObj.carName;
+        imgCar = changeCarController._activeCarObj.imgCar;
+
+        #region Car Level
+        tCarLevel.text = PlayerPrefs.GetInt(changeCarController._activeCarObj.carName + "carLevel") + "/40";
+        fillCarLevel.DOFillAmount((float)PlayerPrefs.GetInt(changeCarController._activeCarObj.carName + "carLevel") / 40, 0.5f);
+        fillEndCarLevel.GetComponent<RectTransform>().DOAnchorPos(new Vector2((float)PlayerPrefs.GetInt(changeCarController._activeCarObj.carName + "carLevel") / 40 * 132f, 0), 0.5f);
+        #endregion
+
+        tDamage.text = "+" + PlayerPrefs.GetFloat(changeCarController._activeCarObj.carName + "carDamage") + "%";
+        tHealth.text = PlayerPrefs.GetFloat(changeCarController._activeCarObj.carName + "carHealth") + "";
+    }
 
     public void ButChangeCar()
     {
