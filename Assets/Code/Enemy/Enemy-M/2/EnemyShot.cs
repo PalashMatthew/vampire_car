e61f40d85458940be7d9fb2d089b9bf581c9b410
MatthewP <PalashM@mail.ru>
2023-12-04 13:35:02 +0300

    fix3

--- Assets/Code/Enemy/Enemy-M/2/EnemyShot.cs
+++ Assets/Code/Enemy/Enemy-M/2/EnemyShot.cs
@@ -14,8 +14,6 @@ public class EnemyShot : MonoBehaviour
 
     private int _enemyShotCount = 1;
 
-    public bool isShotStart;
-
 
     void Initialize()
     {
@@ -28,26 +26,23 @@ public class EnemyShot : MonoBehaviour
     {
         Initialize();
 
+        StartCoroutine(StartShoting());
+    }
+
+    IEnumerator StartShoting()
+    {
+        yield return new WaitForSeconds(1.5f);
+
         StartCoroutine(Shot());
     }
 
     IEnumerator Shot()
     {
-        if (isShotStart)
-        {
-            GameObject _inst = Instantiate(bulletObj, bulletSpawnPoint.position, transform.rotation);
-            _inst.GetComponent<EnemyDefaultBullet>()._gunController = _gunController;
-            _inst.GetComponent<EnemyDefaultBullet>()._controller = gameObject.GetComponent<EnemyController>();
-        }
+        GameObject _inst = Instantiate(bulletObj, bulletSpawnPoint.position, transform.rotation);
+        _inst.GetComponent<EnemyDefaultBullet>()._gunController = _gunController;
+        _inst.GetComponent<EnemyDefaultBullet>()._controller = gameObject.GetComponent<EnemyController>();
 
         yield return new WaitForSeconds(_gunController.shotSpeed * GameObject.Find("GameplayController").GetComponent<WaveController>().waveList[GameObject.Find("GameplayController").GetComponent<WaveController>().currentWave - 1].attackSpeedCoeff);
-
-        if (!isShotStart)
-        {
-            GameObject _inst = Instantiate(bulletObj, bulletSpawnPoint.position, transform.rotation);
-            _inst.GetComponent<EnemyDefaultBullet>()._gunController = _gunController;
-            _inst.GetComponent<EnemyDefaultBullet>()._controller = gameObject.GetComponent<EnemyController>();
-        }
 
         if (_enemyController.carType == EnemyController.CarType.Static)
         {
